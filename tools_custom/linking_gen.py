89f0ed2bd382d24465672d42e4ce03e0f56a80d8
Sribuu <sribuu@Sribuus-MacBook-Pro.local>
2023-06-16 11:05:20 +0700

    - remove some unused variables \n - adjust logic to remove anotation data if has no linking

--- tools_custom/linking_gen.py
+++ tools_custom/linking_gen.py
@@ -6,8 +6,15 @@ import os
 parser = argparse.ArgumentParser()
 
 # Menambahkan argumen yang diharapkan
-parser.add_argument("--labelFile", type=str, default="Label.txt", help="Input label as Label.txt")
-parser.add_argument("--labelOutputFile", type=str, default="Label-linked.txt", help="Generated Label with id and linking. e.g Label-output.txt")
+parser.add_argument(
+    "--labelFile", type=str, default="Label.txt", help="Input label as Label.txt"
+)
+parser.add_argument(
+    "--labelOutputFile",
+    type=str,
+    default="Label-linked.txt",
+    help="Generated Label with id and linking. e.g Label-output.txt",
+)
 
 # Parse argumen dari terminal
 args = parser.parse_args()
@@ -20,42 +27,42 @@ current_directory = os.getcwd()
 
 rootPath = directory
 
-if(directory=="") :
+if directory == "":
     rootPath = current_directory
 
 rootPath = f"{rootPath}/"
-
 filename = f"{rootPath}{args.labelFile}"  # file to read
-
 _dict = {}  # text will be store as dictionary
-_links = []  # text will be store as array of key_linking
 
 # creating dictionary
 with open(filename) as fh:
     for line in fh:
         # reads each line and turn into key value pair dict
-
         command, description = line.split("\t", 1)
-
         _dict[command] = description.strip()
 
         for key, values in _dict.items():
             temp = json.loads(values)  # temporary store array values from dict
 
-            for i in range(len(temp)):
-                temp_data = temp[i]
-                _id = i + 1  # starting from 1
-                temp_data["id"] = _id  # assign id
-                temp_data["linking"] = []  # default value is empty list
-                if "key_cls" in temp_data: #rename key_cls to label if needed
-                    temp_data["label"] = (temp_data.pop("key_cls"))
-                else:
-                    temp_data["label"] = (temp_data["label"])
-                
-                if temp_data["label"] == "None":
-                        temp_data["label"] = "IGNORE"
-                temp[i] = temp_data  # modify current data on temp
+            c = 1  # while counting starts from 1 to allow us safely remove elements from the temp
+            i = c - 1 # real index from array
 
+            while c <= len(temp):
+                temp_data = temp[i]
+
+                if len(temp_data["linking"]) == 0:
+                    del temp[i]  # remove annotation data if it has no linking
+                    continue
+                else:
+                    if "key_cls" in temp_data:  # rename key_cls to label if needed
+                        temp_data["label"] = temp_data.pop("key_cls")
+
+                    if temp_data["label"] == "None":
+                        temp_data["label"] = "IGNORE"
+
+                    temp[i] = temp_data  # modify current data in temp
+
+                c += 1
 
             _dict[key] = json.dumps(temp)  # putting back into _dict
 
@@ -64,4 +71,4 @@ text = "\n".join([f"{key}\t{value}" for key, value in _dict.items()])
 
 # write the text to a file
 with open(f"{rootPath}{args.labelOutputFile}", "w") as file:
-    file.write(text+"\n")
+    file.write(text + "\n")

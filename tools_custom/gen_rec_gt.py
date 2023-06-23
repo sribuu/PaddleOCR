d7e1ab059ddf49fb82798711249531e093861b03
Arief Wijaya <ariefwiijaya@gmail.com>
2023-06-23 07:22:35 +0700

    fix missing p

--- tools_custom/gen_rec_gt.py
+++ tools_custom/gen_rec_gt.py
@@ -2,7 +2,7 @@ import os
 import cv2
 import numpy as np
 import json
-from tools.infer.utility import get_rotate_crop_image
+from paddleocr.tools.infer.utility import get_rotate_crop_image
 import argparse
 
 # Mendapatkan current directory
